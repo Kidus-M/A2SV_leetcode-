f3fc5d45c16a6976849393c8d3819ba4cb8e2f4f
kidus-m <kidus0237@gmail.com>
2025-07-05 21:15:57 +0300

    contest 21

--- Codeforces - A2SV Contest 21/D. Districts Connection.py
+++ Codeforces - A2SV Contest 21/D. Districts Connection.py
@@ -1,1 +1,22 @@
 t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+
+    diff=-1
+    for i in range(1,n):
+        if a[i] !=a[0]:
+            diff =i
+            break
+
+    if diff==-1:
+        print("NO")
+        continue
+    print("YES")
+
+    for i in range(1,n):
+        if a[i] != a[0]:
+            print(1,i+1)
+    for i in range(1,n):
+        if a[i]==a[0] and i != 0:
+            print(diff+1, i +1)
