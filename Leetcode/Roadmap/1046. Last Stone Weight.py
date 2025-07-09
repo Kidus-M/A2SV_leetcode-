16a25ad8ed6303dfb54faaae186d4e2e72488139
kidus-m <kidus0237@gmail.com>
2025-07-09 10:13:26 +0300

    daily leetcode

---
+++ Leetcode/Roadmap/1046. Last Stone Weight.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        for i, stone in enumerate(stones):
+            stones[i] = -stone
+        heapify(stones)
+        while len(stones) > 1:
+            stone1 = -heappop(stones)
+            stone2 = -heappop(stones)
+            if stone1 > stone2:
+                heappush(stones, stone2 - stone1)
+        return -heappop(stones) if len(stones) else 0
