77c388cfe7c4bceb10ba7376d551067c8f7f9b26
kidus-m <kidus0237@gmail.com>
2025-07-12 20:45:37 +0300

    contest 22

--- Codeforces - A2SV Remote Contest 22/C. The Double-Holed Culprit.py
+++ Codeforces - A2SV Remote Contest 22/C. The Double-Holed Culprit.py
@@ -1,3 +1,12 @@
-// C.The
-Double - Holed
-Culprit
+n=int(input())
+p=list(map(int,input().split()))
+
+ans=[]
+for s in range(1,n+1):
+    visited=set()
+    curr=s
+    while curr not in visited:
+        visited.add(curr)
+        curr=p[curr-1]
+    ans.append(curr)
+print(*ans)
