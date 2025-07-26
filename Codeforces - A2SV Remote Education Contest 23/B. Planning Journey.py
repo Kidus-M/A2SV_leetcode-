23506508cbf711e262f68ea1ad35a6f404453534
kidus-m <kidus0237@gmail.com>
2025-07-26 20:41:24 +0300

    contest

--- Codeforces - A2SV Remote Education Contest 23/B. Planning Journey.py
+++ Codeforces - A2SV Remote Education Contest 23/B. Planning Journey.py
@@ -1,2 +1,22 @@
-// B.Planning
-Journey
+t=int(input())
+for _ in range(t):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+
+    count=0
+    ones=sum(a[:k])
+    i=0
+
+    while i <= n-k:
+        if ones==0:
+            count += 1
+            i+=k+1
+            if i <= n-k:
+                ones=sum(a[i:i+k])
+        else:
+            if i +k<n:
+                ones-=a[i]
+                ones += a[i+k]
+            i += 1
+
+    print(count)
