692539b9817616178b23ad798ba198a2af8fb1c0
kidus-m <kidus0237@gmail.com>
2025-07-26 20:47:56 +0300

    contest

--- Codeforces - A2SV Remote Education Contest 23/C. Sort.py
+++ Codeforces - A2SV Remote Education Contest 23/C. Sort.py
@@ -1,1 +1,20 @@
-// C.Sort
+t=int(input())
+for _ in range(t):
+    n, c = map(int, input().split())
+    a = list(map(int, input().split()))
+
+
+    cost=[]
+    for i in range(n):
+        cost.append(a[i]+(i+1))
+    cost.sort()
+
+    used=0
+    for x in cost:
+        if x<=c:
+            used += 1
+            c-=x
+        else:
+            break
+
+    print(used)
