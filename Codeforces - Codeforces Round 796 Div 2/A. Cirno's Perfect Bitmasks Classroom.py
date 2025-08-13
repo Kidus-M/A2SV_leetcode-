81f435cae7e11eb1292daf844527e1fcda4d6481
kidus-m <kidus0237@gmail.com>
2025-08-13 20:14:03 +0300

    daily

--- Codeforces - Codeforces Round 796 Div 2/A. Cirno's Perfect Bitmasks Classroom.py
+++ Codeforces - Codeforces Round 796 Div 2/A. Cirno's Perfect Bitmasks Classroom.py
@@ -1,2 +1,9 @@
-// A.Cirno
-'s Perfect Bitmasks Classroom
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    if x == 1:
+        print(3)
+    elif (x & (x - 1)) == 0:
+        print(x + 1)
+    else:
+        print(x & -x)
