5c3395090783a2ed43350a7293ef012d6e414b20
kidus-m <kidus0237@gmail.com>
2025-08-14 20:54:35 +0300

    daily

--- AtCoder - Educational DP Contest/E - Knapsack 2.py
+++ AtCoder - Educational DP Contest/E - Knapsack 2.py
@@ -1,2 +1,14 @@
-// E - Knapsack
-2
+INF = float('inf')
+N, W = map(int, input().split())
+items = [tuple(map(int, input().split())) for _ in range(N)]
+max_value_sum = sum(v for _, v in items)
+dp = [INF] * (max_value_sum + 1)
+dp[0] = 0
+for w, v in items:
+    for val in range(max_value_sum, v - 1, -1):
+        dp[val] = min(dp[val], dp[val - v] + w)
+ans = 0
+for val in range(max_value_sum + 1):
+    if dp[val] <= W:
+        ans = val
+print(ans)
