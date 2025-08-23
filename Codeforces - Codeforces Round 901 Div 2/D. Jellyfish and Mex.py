b61b94c0edcbfd20158754126c778f207ba1f4ee
kidus-m <kidus0237@gmail.com>
2025-08-23 09:11:06 +0300

    track questions

--- Codeforces - Codeforces Round 901 Div 2/D. Jellyfish and Mex.py
+++ Codeforces - Codeforces Round 901 Div 2/D. Jellyfish and Mex.py
@@ -1,1 +1,17 @@
-// D.Jellyfish and Mex
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    s=set(a)
+    mex=0
+    while mex in s:
+        mex+=1
+    freq={}
+    for x in a:
+        if x<=mex:
+            freq[x]=freq.get(x,0)+1
+    ans=mex
+    for i in range(mex):
+        if freq.get(i,0)>1:
+            ans+=freq[i]-1
+    print(ans)
