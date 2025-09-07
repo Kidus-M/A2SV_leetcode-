3e5d0a0f94bd5112c9f5f0d4f4cae2c573a6ae09
kidus-m <kidus0237@gmail.com>
2025-09-07 12:59:37 +0300

    track

--- Codeforces - A2SV Contest 29/B. Game of Mathletes.py
+++ Codeforces - A2SV Contest 29/B. Game of Mathletes.py
@@ -1,2 +1,18 @@
-//  B. Game of Mathletes
-
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    x = list(map(int, input().split()))
+    freq = [0] * (n + 1)
+    for num in x:
+        freq[num] += 1
+    score = 0
+    for i in range(1, n + 1):
+        if k - i >= 1 and k - i <= n:
+            if i == k - i:
+                score += freq[i] // 2
+            else:
+                pairs = min(freq[i], freq[k - i])
+                score += pairs
+                freq[i] -= pairs
+                freq[k - i] -= pairs
+    print(score)
