b3e230051226005deb714b0e4c999ffc0efae58a
kidus-m <kidus0237@gmail.com>
2025-09-07 12:54:57 +0300

    track

--- Codeforces - A2SV Contest 29/A. Zhan's Blender.py
+++ Codeforces - A2SV Contest 29/A. Zhan's Blender.py
@@ -1,2 +1,11 @@
-// A.Zhan
-'s Blender
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    x, y = map(int, input().split())
+    if n == 0:
+        print(0)
+        continue
+    batches = (n + y - 1) // y
+    fruits_per_batch = min(x, y)
+    full_seconds = (n + fruits_per_batch - 1) // fruits_per_batch
+    print(max(batches, full_seconds))
