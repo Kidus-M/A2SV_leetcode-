cdd4382be193f418c1bcaa9b164f5cb623c48bac
kidus-m <kidus0237@gmail.com>
2025-09-07 13:10:02 +0300

    track

--- Codeforces - A2SV Contest 29/C. Binary Deque.py
+++ Codeforces - A2SV Contest 29/C. Binary Deque.py
@@ -1,2 +1,25 @@
-// C.Binary
-Deque
+t=int(input())
+for _ in range(t):
+    n,s=map(int,input().split())
+    a=list(map(int,input().split()))
+    tot=sum(a)
+    if tot<s:
+        print(-1)
+        continue
+    if tot==s:
+        print(0)
+        continue
+    left=0
+    cur=0
+    best=-1
+    for right in range(n):
+        cur+=a[right]
+        while cur>s and left<=right:
+            cur-=a[left]
+            left+=1
+        if cur==s:
+            best=max(best,right-left+1)
+    if best==-1:
+        print(-1)
+    else:
+        print(n-best)
