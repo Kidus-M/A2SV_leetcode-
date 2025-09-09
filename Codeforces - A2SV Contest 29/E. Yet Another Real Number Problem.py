6597ecb39814e153720e436f65b827024d86fae9
kidus-m <kidus0237@gmail.com>
2025-09-09 10:02:33 +0300

    track

--- Codeforces - A2SV Contest 29/E. Yet Another Real Number Problem.py
+++ Codeforces - A2SV Contest 29/E. Yet Another Real Number Problem.py
@@ -1,25 +1,29 @@
+import sys
 
-MOD = 10**9 + 7
+MOD = 10 ** 9 + 7
+
 
 def power(base, exponent):
     if exponent == 0:
         return 1
-    half = power(base, exponent//2)
+    half = power(base, exponent // 2)
     if exponent % 2 == 0:
-        return half *  half % MOD
+        return half * half % MOD
     else:
         return base * half * half % MOD
 
 
 def helper(x):
+    #  number of leading zero means
+    #  the number of times i can devide the number by 2
     two_division_count = 0
     while x & 1 == 0:
         x >>= 1
         two_division_count += 1
     return two_division_count
 
-def solve():
 
+def solve():
     test = int(input())
     for _ in range(test):
         n = int(input())
@@ -49,7 +53,7 @@ def solve():
             res = total_sum
             for value, exponent in stack:
                 res += power(2, exponent) * value % MOD
-            ans.append(res)
+            ans.append(res % MOD)
         print(*ans)
 
 
