2517948407c539ae406b6a7f0d0c039d271a78c0
kidus-m <kidus0237@gmail.com>
2025-09-14 12:04:41 +0300

    track

---
+++ Leetcode/Second Track/2470. Number of Subarrays With LCM Equal to K.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarrayLCM(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        count = 0
+
+        for i in range(n):
+            lcm_val = 1
+
+            for j in range(i,n):
+                lcm_val = (lcm_val * nums[j]) // math.gcd(lcm_val, nums[j])
+
+                if lcm_val == k:
+                    count += 1
+                elif lcm_val >= k:
+                    break
+        return count
