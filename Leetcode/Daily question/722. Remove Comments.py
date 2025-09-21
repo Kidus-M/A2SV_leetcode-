c4df3c781f3d4b1a1f1483ae3c292afc114e42e0
kidus-m <kidus0237@gmail.com>
2025-09-21 09:54:58 +0300

    track

---
+++ Leetcode/Daily question/722. Remove Comments.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def removeComments(self, source):
+        """
+        :type source: List[str]
+        :rtype: List[str]
+        """
+        ans = []
+        inblock = False
+        for line in source:
+            if not inblock:
+                ans.append([])
+            i = 0
+            while i < len(line):
+                if not inblock:
+                    if line[i] == '/' and i + 1 < len(line) and line[i+1] in '/*':
+                        if line[i+1] == '/':
+                            break
+                        i += 1
+                        inblock = True
+                    else:
+                        ans[-1].append(line[i])
+                else:
+                    if line[i] == '*' and i + 1 < len(line) and line[i+1] == '/':
+                        i += 1
+                        inblock = False
+                i += 1
+        return [''.join(line) for line in ans if line]
