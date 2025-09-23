137c200970a5f01b067720f9d9ec606241d872aa
kidus-m <kidus0237@gmail.com>
2025-09-23 08:31:07 +0300

    commit

--- Codeforces - ITMO Academy pilot course - Two Pointers Method - Step 2/A. Segment with Small Sum.py
+++ Codeforces - ITMO Academy pilot course - Two Pointers Method - Step 2/A. Segment with Small Sum.py
@@ -1,2 +1,17 @@
-// A.Segment
-with Small Sum
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
+
+left = 0
+current_sum = 0
+max_length = 0
+
+for right in range(n):
+    current_sum += a[right]
+
+    while current_sum > s and left <= right:
+        current_sum -= a[left]
+        left += 1
+
+    max_length = max(max_length, right - left + 1)
+
+print(max_length)
