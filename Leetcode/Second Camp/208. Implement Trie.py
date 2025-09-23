1bbbc30d0829e9407b69d04ac2914798b51c18cc
kidus-m <kidus0237@gmail.com>
2025-09-23 22:31:41 +0300

    traacking

---
+++ Leetcode/Second Camp/208. Implement Trie.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = [ None for _ in range(26) ]
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            index = ord(char) - ord('a')
+            if node.children[index] is None:
+                node.children[index] = TrieNode()
+            node = node.children[index]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            index = ord(char) - ord('a')
+            if node.children[index] is None:
+                return False
+            node = node.children[index]
+        return node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            index = ord(char) - ord('a')
+            if node.children[index] is None:
+                return False
+            node = node.children[index]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
