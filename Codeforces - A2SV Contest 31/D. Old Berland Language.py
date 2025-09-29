d9b1ac7d6c4beedcb3b6f08b59f202a3d5925c38
kidus-m <kidus0237@gmail.com>
2025-09-29 10:03:44 +0300

    daily

--- Codeforces - A2SV Contest 31/D. Old Berland Language.py
+++ Codeforces - A2SV Contest 31/D. Old Berland Language.py
@@ -1,13 +1,43 @@
+import sys
+class TrieNode:
+    def __init__(self):
+        self.children = [None, None]
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def can_insert(self, s):
+        node = self.root
+        for i, c in enumerate(s):
+            bit = int(c)
+            if not node.children[bit]:
+                if node.is_end:
+                    return False
+                node.children[bit] = TrieNode()
+            node = node.children[bit]
+            if i + 1 == len(s) and (node.is_end or node.children[0] or node.children[1]):
+                return False
+        return True
+
+    def insert(self, s):
+        node = self.root
+        for c in s:
+            bit = int(c)
+            node = node.children[bit]
+        node.is_end = True
+input=sys.stdin.readline
 n = int(input())
 lengths = list(map(int, input().split()))
-used = set()
+trie = Trie()
 words = []
 for l in lengths:
     found = False
     for i in range(1 << l):
         s = bin(i)[2:].zfill(l)
-        if all(not s.startswith(w) and not w.startswith(s) for w in used):
-            used.add(s)
+        if trie.can_insert(s):
+            trie.insert(s)
             words.append(s)
             found = True
             break
