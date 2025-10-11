585a7c7501dec3510aad58ed9e992c1cf048a861
kidus-m <kidus0237@gmail.com>
2025-10-11 12:02:28 +0300

    camping

---
+++ Leetcode/Second Camp/1311. Get Watched Videos by Your Friends.py
@@ -0,0 +1,25 @@
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> \
+    List[str]:
+        n = len(friends)
+        visited = [False] * n
+
+        visited[id] = True
+        q = deque([id])
+        curr = 0
+
+        while q and curr < level:
+            for _ in range(len(q)):
+                person = q.popleft()
+                for f in friends[person]:
+                    if not visited[f]:
+                        visited[f] = True
+                        q.append(f)
+
+            curr += 1
+
+        video = Counter()
+        for f in q:
+            video.update(watchedVideos[f])
+        ans = sorted(video.keys(), key=lambda x: (video[x], x))
+        return ans
