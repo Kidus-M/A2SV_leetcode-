1b446b712a04bd08ce75e61601d8823854a266aa
kidus-m <kidus0237@gmail.com>
2025-10-11 22:50:20 +0300

    contest

--- Codeforces - A2SV Contest 33/D. Shortest Path Problem.py
+++ Codeforces - A2SV Contest 33/D. Shortest Path Problem.py
@@ -1,3 +1,39 @@
-// D.Shortest
-Path
-Problem?
+n, m = map(int, input().split())
+graph=[[] for i in range(n+1)]
+edges=[]
+for _ in range(m):
+    a,b,w = map(int, input().split())
+    graph[a].append((b,w))
+    graph[b].append((a,w))
+    edges.append((a,b,w))
+
+from collections import deque
+dist=[-1]*(n+1)
+dist[1]=0
+
+q=deque([1])
+
+
+while q:
+    u=q.popleft()
+    for v, w in graph[u]:
+        if dist[v]==-1:
+            dist[v]=dist[u]^w
+            q.append(v)
+
+basis=[]
+for a,b,w in edges:
+    val = dist[a] ^ dist[b] ^ w
+    if val ==0:
+        continue
+    for b in basis:
+        val = min(val, val ^ b)
+    if val:
+        basis.append(val)
+        basis.sort(reverse=True)
+
+
+ans=dist[n]
+for b in basis:
+    ans=min(ans^b,ans)
+print(ans)
