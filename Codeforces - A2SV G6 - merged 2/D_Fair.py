5bca6e1bd630f5f4f2482a976d29b1cac77467c7
kidus-m <kidus0237@gmail.com>
2025-11-08 22:25:39 +0300

    fair

--- Codeforces - A2SV G6 - merged 2/D_Fair.py
+++ Codeforces - A2SV G6 - merged 2/D_Fair.py
@@ -1,6 +1,7 @@
 from collections import deque
 import sys
 
+
 input=sys.stdin.readline
 n,m,k,s=map(int,input().split())
 goods=list(map(int,input().split()))
@@ -16,21 +17,24 @@ for _ in range(m):
     graph[v].append(u)
 
 
-dist=[[10**9]*k for _ in range(n)]
+dist=[[] for _ in range(n)]
 
-for g in range(k):
+for g in range(1,k+1):
     q=deque()
-
+    visited=[-1]*n
     for i in range(n):
-        if goods[i]==g+1:
-            dist[i][g]=0
+        if goods[i]==g:
+            visited[i]=0
             q.append(i)
     while q:
         u=q.popleft()
         for v in graph[u]:
-            if dist[v][g]>dist[u][g]+1:
-                dist[v][g]=dist[u][g]+1
+            if visited[v]==-1:
+                visited[v]=visited[u]+1
                 q.append(v)
+    for i in range(n):
+        if visited[i]!=-1:
+            dist[i].append(visited[i])
 
 
 ans=[]
