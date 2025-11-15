97d65f8f2fb1a6516c27d7393d6efe367c7dfbe0
kidus-m <kidus0237@gmail.com>
2025-11-15 22:32:10 +0300

    contest

--- Codeforces - A2SV G6 - merged 3/E_Nikita_and_LCM.py
+++ Codeforces - A2SV G6 - merged 3/E_Nikita_and_LCM.py
@@ -1,1 +1,16 @@
-// E.Nikita and LCM
+import math
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    arr=list(map(int,input().split()))
+
+    L=1
+    for x in arr:
+        L=L*x//math.gcd(L,x)
+
+
+        if L>10**18:
+            break
+    ans=sum(1 for x in arr if L%x==0)
+    print(ans)
