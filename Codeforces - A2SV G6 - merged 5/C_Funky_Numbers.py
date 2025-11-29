126227a55278a8e21990bf855c3e2bad200f7bfe
kidus-m <kidus0237@gmail.com>
2025-11-29 20:44:20 +0300

    daily

--- Codeforces - A2SV G6 - merged 5/C_Funky_Numbers.py
+++ Codeforces - A2SV G6 - merged 5/C_Funky_Numbers.py
@@ -1,2 +1,27 @@
-// C.Funky
-Numbers
+t=int(input())
+
+
+tri=[]
+k=1
+while True:
+    x=k*(k+1)//2
+    if x >t:
+        break
+    tri.append(x)
+    k += 1
+
+
+i, j=0, len(tri)-1
+ok=False
+
+while i <=j:
+    s=tri[i]+tri[j]
+    if s==t:
+        ok=True
+        break
+    elif s<t:
+        i += 1
+    else:
+        j-=1
+
+print("YES" if ok else "NO")
