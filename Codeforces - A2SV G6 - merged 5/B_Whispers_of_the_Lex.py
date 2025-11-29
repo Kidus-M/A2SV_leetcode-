b5cd2a4e6272488530516a42017cd9be10582cd6
kidus-m <kidus0237@gmail.com>
2025-11-29 20:31:14 +0300

    daily

--- Codeforces - A2SV G6 - merged 5/B_Whispers_of_the_Lex.py
+++ Codeforces - A2SV G6 - merged 5/B_Whispers_of_the_Lex.py
@@ -1,4 +1,17 @@
-// B.Whispers
-of
-the
-Lex
+n,d=map(int,input().split())
+
+
+need= n-1
+cost=0
+
+take=min(d, need)
+cost += take
+need -= take
+
+city=2
+
+while need >0:
+    cost += city
+    city += 1
+    need -= 1
+print(cost)
