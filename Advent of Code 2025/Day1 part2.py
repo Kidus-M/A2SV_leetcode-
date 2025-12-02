21f3fafd06419263356cde81410134cd23722dda
kidus-m <kidus0237@gmail.com>
2025-12-02 20:30:53 +0300

    advent

--- Advent of Code 2025/Day1 part2.py
+++ Advent of Code 2025/Day1 part2.py
@@ -1,7 +1,3 @@
-# aoc_day1_part2.py
-# Counts every time the dial points at 0 during any click (including the end of a rotation).
-# Run and paste instructions, then press Enter on an empty line (or send EOF/Ctrl-D).
-
 pos = 50
 count_zero = 0
 
@@ -18,21 +14,17 @@ while True:
     amount = int(line[1:])
 
     if direction == "R":
-        # k such that (pos + k) % 100 == 0  => k ≡ (100 - pos) % 100
         k0 = (100 - pos) % 100
         if k0 == 0:
             k0 = 100
-    else:  # L
-        # k such that (pos - k) % 100 == 0  => k ≡ pos % 100
+    else:
         k0 = pos % 100
         if k0 == 0:
             k0 = 100
 
     if amount >= k0:
-        # number of k in {1..amount} that are ≡ k0 (mod 100)
         count_zero += 1 + (amount - k0) // 100
 
-    # update position after the rotation
     if direction == "R":
         pos = (pos + amount) % 100
     else:
