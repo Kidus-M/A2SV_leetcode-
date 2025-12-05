746db9dda7f8312b7480215281b2dffead767cf7
kidus-m <kidus0237@gmail.com>
2025-12-05 16:01:47 +0300

    advent day 5

--- Advent of Code 2025/Day5.py
+++ Advent of Code 2025/Day5.py
@@ -1,5 +1,4 @@
-print("Enter the ingredient ID ranges, then a blank line, then the IDs to check.")
-print("Waiting for input...")
+print("Processing...")
 
 ranges = []
 while True:
@@ -9,26 +8,19 @@ while True:
     a, b = map(int, line.split("-"))
     ranges.append((a, b))
 
-print("Ranges loaded:", ranges)
-print("Now enter ingredient IDs to test (end with CTRL+D / CTRL+Z):")
-
-fresh_count = 0
+fresh = 0
+ids = []
 
 while True:
     try:
-        x = int(input().strip())
-        print("Checking:", x)
-        is_fresh = False
-        for a, b in ranges:
-            if a <= x <= b:
-                is_fresh = True
-                break
-        if is_fresh:
-            print(x, "is FRESH")
-            fresh_count += 1
-        else:
-            print(x, "is SPOILED")
+        ids.append(int(input().strip()))
     except EOFError:
         break
 
-print("Total fresh ingredients:", fresh_count)
+for x in ids:
+    for a, b in ranges:
+        if a <= x <= b:
+            fresh += 1
+            break
+
+print(fresh)
