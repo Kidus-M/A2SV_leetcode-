05ebc99943e8efc3da1ed24b485455d58af24dfb
kidus-m <kidus0237@gmail.com>
2025-12-14 12:10:46 +0300

    daily

--- Daily Leetcode Challenge 2026/2147. Number of Ways to Divide a Long Corridor DEC 14,2025.py
+++ Daily Leetcode Challenge 2026/2147. Number of Ways to Divide a Long Corridor DEC 14,2025.py
@@ -5,11 +5,9 @@ class Solution:
         total_seats = corridor.count("S")
         if total_seats == 0 or total_seats % 2 == 1:
             return 0
-
         ans = 1
         seat = 0
         plant = 0
-
         for c in corridor:
             if c == "S":
                 if seat == 2:
@@ -20,5 +18,4 @@ class Solution:
             else:
                 if seat == 2:
                     plant += 1
-
         return ans
