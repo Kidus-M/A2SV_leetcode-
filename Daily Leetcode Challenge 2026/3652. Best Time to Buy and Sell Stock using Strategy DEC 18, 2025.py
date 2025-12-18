c78ac04fd545bc21d52b267e4b0bdc03d05342a0
kidus-m <kidus0237@gmail.com>
2025-12-18 09:19:42 +0300

    daily leetcode

--- Daily Leetcode Challenge 2026/3652. Best Time to Buy and Sell Stock using Strategy DEC 18, 2025.py
+++ Daily Leetcode Challenge 2026/3652. Best Time to Buy and Sell Stock using Strategy DEC 18, 2025.py
@@ -2,17 +2,14 @@ class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
         n, h = len(prices), k >> 1
         base = prev = nxt = best = 0
-
         for i in range(n):
             base += strategy[i] * prices[i]
-
         for i in range(k):
             prev += strategy[i] * prices[i]
             if i >= h:
                 nxt += prices[i]
 
         best = max(0, nxt - prev)
-
         for r in range(k, n):
             l = r - k + 1
             prev += strategy[r] * prices[r] - strategy[l - 1] * prices[l - 1]
