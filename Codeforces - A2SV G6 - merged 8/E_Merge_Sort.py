423f2854dde360f3f37a402dabff5a67e4541cd2
kidus-m <kidus0237@gmail.com>
2025-12-20 20:41:24 +0300

    contest

--- Codeforces - A2SV G6 - merged 8/E_Merge_Sort.py
+++ Codeforces - A2SV G6 - merged 8/E_Merge_Sort.py
@@ -1,35 +1,37 @@
 import sys
 
 
-def construct(l, r, perm, val):
+def build(l, r, swaps_left, perm, val):
     if l >= r:
-        return
+        return swaps_left
     if l + 1 == r:
         perm[l] = val[0]
         val[0] += 1
-        return
+        return swaps_left
 
     mid = (l + r) // 2
 
-    construct(l, mid, perm, val)
-    construct(mid, r, perm, val)
+    swaps_left = build(l, mid, swaps_left, perm, val)
+    swaps_left = build(mid, r, swaps_left, perm, val)
 
-    if remaining[0] > 0:
-        remaining[0] -= 1
+    if swaps_left > 0:
         perm[mid - 1], perm[mid] = perm[mid], perm[mid - 1]
+        swaps_left -= 1
+
+    return swaps_left
 
 
 input = sys.stdin.readline
 n, k = map(int, input().split())
 
-if k % 2 == 0 or k > 2 * n - 1:
+if k % 2 == 0 or k > 2 * n - 1 or k < 1:
     print(-1)
-    sys.exit()
-
-perm = [0] * n
-val = [1]
-remaining = [k - 1]
-
-construct(0, n, perm, val)
-
-print(' '.join(map(str, perm)))
+else:
+    perm = [0] * n
+    val = [1]
+    remaining_swaps = k - 1
+    final_swaps = build(0, n, remaining_swaps, perm, val)
+    if final_swaps != 0:
+        print(-1)
+    else:
+        print(' '.join(map(str, perm)))
