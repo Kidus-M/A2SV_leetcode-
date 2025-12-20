0071c2f531cf1ba631239103dba1f43a039d3303
kidus-m <kidus0237@gmail.com>
2025-12-20 20:38:46 +0300

    contest

--- Codeforces - A2SV G6 - merged 8/E_Merge_Sort.py
+++ Codeforces - A2SV G6 - merged 8/E_Merge_Sort.py
@@ -1,28 +1,35 @@
 import sys
 
-def construct(l, r, remaining_k, perm):
-    if r - l <= 0:
+
+def construct(l, r, perm, val):
+    if l >= r:
         return
-    if r - l == 1:
-        perm[l] = current[0]
-        current[0] += 1
+    if l + 1 == r:
+        perm[l] = val[0]
+        val[0] += 1
         return
-    mid = l + (r - l) // 2
-    if remaining_k > 0:
-        construct(mid, r, remaining_k - 1, perm)
-        construct(l, mid, 0, perm)
+
+    mid = (l + r) // 2
+
+    construct(l, mid, perm, val)
+    construct(mid, r, perm, val)
+
+    if remaining[0] > 0:
+        remaining[0] -= 1
         perm[mid - 1], perm[mid] = perm[mid], perm[mid - 1]
-    else:
-        construct(l, mid, 0, perm)
-        construct(mid, r, 0, perm)
+
 
 input = sys.stdin.readline
 n, k = map(int, input().split())
 
 if k % 2 == 0 or k > 2 * n - 1:
     print(-1)
-else:
-    perm = [0] * n
-    current = [1]
-    construct(0, n, k - 1, perm)
-    print(' '.join(map(str, perm)))
+    sys.exit()
+
+perm = [0] * n
+val = [1]
+remaining = [k - 1]
+
+construct(0, n, perm, val)
+
+print(' '.join(map(str, perm)))
