e669c2db1a54bfbe746c15adabf78c80126e7fee
kidus-m <kidus0237@gmail.com>
2025-12-20 20:34:37 +0300

    contests

--- Codeforces - A2SV G6 - merged 8/D_Sagheer_and_Nubian_Market.py
+++ Codeforces - A2SV G6 - merged 8/D_Sagheer_and_Nubian_Market.py
@@ -1,2 +1,26 @@
-// D.Sagheer and Nubian
-Market
+import sys
+input = sys.stdin.readline
+
+n, S = map(int, input().split())
+a = list(map(int, input().split()))
+
+low = 0
+high = n
+best_k = 0
+best_cost = 0
+
+while low <= high:
+    mid = (low + high) // 2
+    costs = []
+    for i in range(n):
+        costs.append(a[i] + (i + 1) * mid)
+    costs.sort()
+    total = sum(costs[:mid])
+    if total <= S:
+        best_k = mid
+        best_cost = total
+        low = mid + 1
+    else:
+        high = mid - 1
+
+print(best_k, best_cost)
