e7d8976ce13e0f46a8157703e62e34d78defaff4
kidus-m <kidus0237@gmail.com>
2025-12-22 09:35:00 +0300

    columns daily question

--- Daily Leetcode Challenge 2026/960. Delete Columns to Make Sorted III DEC 22, 2025.py
+++ Daily Leetcode Challenge 2026/960. Delete Columns to Make Sorted III DEC 22, 2025.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(strs[0])
         m = len(strs)
         dp = [1] * n
-
         for i in range(1, n):
             for j in range(i):
                 ok = True
@@ -13,7 +12,6 @@ class Solution:
                         break
                 if ok:
                     dp[i] = max(dp[i], dp[j] + 1)
-
         mx = 0
         for v in dp:
             mx = max(mx, v)
