fce23b21c9cee1fbe17efdf6a3196c41769223e7
kidus-m <kidus0237@gmail.com>
2025-12-24 14:24:34 +0300

    daily

--- Daily Leetcode Challenge 2026/3074. Apple Redistribution into Boxes DEC 24, 2025.py
+++ Daily Leetcode Challenge 2026/3074. Apple Redistribution into Boxes DEC 24, 2025.py
@@ -2,7 +2,6 @@ class Solution:
     def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
         total = sum(apple)
         capacity.sort(reverse = True)
-
         for i, c in enumerate(capacity):
             total -= c
             if(total <= 0):
