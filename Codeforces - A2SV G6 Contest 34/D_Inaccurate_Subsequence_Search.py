4dec3a8b7117bb9fe7e9eb24c9bf10447b3e56d9
kidus-m <kidus0237@gmail.com>
2025-12-27 21:21:33 +0300

    contest

--- Codeforces - A2SV G6 Contest 34/D_Inaccurate_Subsequence_Search.py
+++ Codeforces - A2SV G6 Contest 34/D_Inaccurate_Subsequence_Search.py
@@ -12,24 +12,19 @@ for _ in range(t):
     cnt_b = defaultdict(int)
     for x in b:
         cnt_b[x] += 1
-
     cnt_w = defaultdict(int)
     matches = 0
     ans = 0
-
     for i in range(m):
         x = a[i]
         cnt_w[x] += 1
         if cnt_w[x] <= cnt_b[x]:
             matches += 1
-
     if matches >= k:
         ans += 1
-
     for i in range(m, n):
         add = a[i]
         rem = a[i - m]
-
         cnt_w[add] += 1
         if cnt_w[add] <= cnt_b[add]:
             matches += 1
@@ -37,7 +32,6 @@ for _ in range(t):
         if cnt_w[rem] <= cnt_b[rem]:
             matches -= 1
         cnt_w[rem] -= 1
-
         if matches >= k:
             ans += 1
 
