af6663df76339904ad31d030ab2ff674f1243c03
kidus-m <kidus0237@gmail.com>
2025-12-27 21:20:55 +0300

    contest

--- Codeforces - A2SV G6 Contest 34/D_Inaccurate_Subsequence_Search.py
+++ Codeforces - A2SV G6 Contest 34/D_Inaccurate_Subsequence_Search.py
@@ -1,3 +1,44 @@
-// D.Inaccurate
-Subsequence
-Search
+import sys
+from collections import defaultdict
+
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n, m, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    cnt_b = defaultdict(int)
+    for x in b:
+        cnt_b[x] += 1
+
+    cnt_w = defaultdict(int)
+    matches = 0
+    ans = 0
+
+    for i in range(m):
+        x = a[i]
+        cnt_w[x] += 1
+        if cnt_w[x] <= cnt_b[x]:
+            matches += 1
+
+    if matches >= k:
+        ans += 1
+
+    for i in range(m, n):
+        add = a[i]
+        rem = a[i - m]
+
+        cnt_w[add] += 1
+        if cnt_w[add] <= cnt_b[add]:
+            matches += 1
+
+        if cnt_w[rem] <= cnt_b[rem]:
+            matches -= 1
+        cnt_w[rem] -= 1
+
+        if matches >= k:
+            ans += 1
+
+    print(ans)
