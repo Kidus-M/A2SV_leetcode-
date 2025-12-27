01dabadb29fd30dd26e0adfccd695084d167ac70
kidus-m <kidus0237@gmail.com>
2025-12-27 21:02:37 +0300

    contest

--- Codeforces - A2SV G6 Contest 34/C_Fancy_Coins.py
+++ Codeforces - A2SV G6 Contest 34/C_Fancy_Coins.py
@@ -4,11 +4,10 @@ input = sys.stdin.readline
 
 t = int(input())
 for _ in range(t):
-    m,k,a1,ak=map(int,input().split())
-    useRegular=min(a1,m)
-    remaining=m-useRegular
-
-    if m<=a1:
-        print(0)
-    else:
-        print((m-a1+k-1)//k)
+    m, k, a1, ak = map(int, input().split())
+    use_k = min(ak, m // k)
+    m -= use_k * k
+    use_1 = min(a1, m)
+    m -= use_1
+    fancy = (m + k - 1) // k
+    print(fancy)
