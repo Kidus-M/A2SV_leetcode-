64c2bed595388708687be6ffcd2206cb006ba377
kidus-m <kidus0237@gmail.com>
2025-12-28 12:23:25 +0300

    daily

--- Codeforces - A2SV G6 Contest 34/D_Inaccurate_Subsequence_Search.py
+++ Codeforces - A2SV G6 Contest 34/D_Inaccurate_Subsequence_Search.py
@@ -1,37 +1,47 @@
 import sys
-from collections import defaultdict
-
 input = sys.stdin.readline
 
 t = int(input())
+
 for _ in range(t):
     n, m, k = map(int, input().split())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
 
-    cnt_b = defaultdict(int)
+    cnt_b = {}
     for x in b:
-        cnt_b[x] += 1
-    cnt_w = defaultdict(int)
+        cnt_b[x] = cnt_b.get(x, 0) + 1
+
+    cnt_w = {}
     matches = 0
     ans = 0
+
+    # Initial window
     for i in range(m):
         x = a[i]
-        cnt_w[x] += 1
-        if cnt_w[x] <= cnt_b[x]:
-            matches += 1
+        if x in cnt_b:
+            cnt_w[x] = cnt_w.get(x, 0) + 1
+            if cnt_w[x] <= cnt_b[x]:
+                matches += 1
+
     if matches >= k:
         ans += 1
+
+    # Sliding window
     for i in range(m, n):
         add = a[i]
         rem = a[i - m]
-        cnt_w[add] += 1
-        if cnt_w[add] <= cnt_b[add]:
-            matches += 1
 
-        if cnt_w[rem] <= cnt_b[rem]:
-            matches -= 1
-        cnt_w[rem] -= 1
+        if add in cnt_b:
+            cnt_w[add] = cnt_w.get(add, 0) + 1
+            if cnt_w[add] <= cnt_b[add]:
+                matches += 1
+
+        if rem in cnt_b:
+            if cnt_w[rem] <= cnt_b[rem]:
+                matches -= 1
+            cnt_w[rem] -= 1
+
         if matches >= k:
             ans += 1
 
