6fb93199bc1776ebd07d65d46ae56283653e69e2
kidus-m <kidus0237@gmail.com>
2026-01-03 20:48:10 +0300

    contest

--- Codeforces - A2SV G6 Contest 35/D_Epic_Transformation.py
+++ Codeforces - A2SV G6 Contest 35/D_Epic_Transformation.py
@@ -1,2 +1,12 @@
-// D.Epic
-Transformation
+from collections import Counter
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    freq = Counter(a)
+    mx = max(freq.values())
+
+    print(max(0, 2 * mx - n))
