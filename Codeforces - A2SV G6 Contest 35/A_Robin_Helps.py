a63664877d48a45b872dc91200401a43948f7ad4
kidus-m <kidus0237@gmail.com>
2026-01-03 20:22:31 +0300

    contest

--- Codeforces - A2SV G6 Contest 35/A_Robin_Helps.py
+++ Codeforces - A2SV G6 Contest 35/A_Robin_Helps.py
@@ -1,2 +1,17 @@
-// A.Robin
-Helps
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    gold = 0
+    given = 0
+
+    for x in a:
+        if x >= k:
+            gold += x
+        elif x == 0 and gold > 0:
+            gold -= 1
+            given += 1
+
+    print(given)
