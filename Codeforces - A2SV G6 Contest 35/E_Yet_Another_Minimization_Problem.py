ec7af08172dac832b2526e1566a6981f3d84e5b0
kidus-m <kidus0237@gmail.com>
2026-01-03 20:50:16 +0300

    contest

--- Codeforces - A2SV G6 Contest 35/E_Yet_Another_Minimization_Problem.py
+++ Codeforces - A2SV G6 Contest 35/E_Yet_Another_Minimization_Problem.py
@@ -1,4 +1,25 @@
-// E.Yet
-Another
-Minimization
-Problem
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    total_sum = sum(a) + sum(b)
+    sq_sum = sum(x * x for x in a) + sum(x * x for x in b)
+
+    dp = {0}
+
+    for i in range(n):
+        new_dp = set()
+        for s in dp:
+            new_dp.add(s + a[i])
+            new_dp.add(s + b[i])
+        dp = new_dp
+
+    best = float('inf')
+    for s in dp:
+        best = min(best, s * s + (total_sum - s) * (total_sum - s))
+
+    answer = best + (n - 2) * sq_sum
+    print(answer)
