10d8bf1fee3f8889372dbf5110eb25d1510e5ca4
kidus-m <kidus0237@gmail.com>
2026-01-03 20:33:41 +0300

    contest

--- Codeforces - A2SV G6 Contest 35/B_Sushi_for_Two.py
+++ Codeforces - A2SV G6 Contest 35/B_Sushi_for_Two.py
@@ -1,2 +1,19 @@
-// B.Sushi
-for Two
+n = int(input())
+t = list(map(int, input().split()))
+
+groups = []
+count = 1
+
+for i in range(1, n):
+    if t[i] == t[i - 1]:
+        count += 1
+    else:
+        groups.append(count)
+        count = 1
+groups.append(count)
+
+ans = 0
+for i in range(len(groups) - 1):
+    ans = max(ans, 2 * min(groups[i], groups[i + 1]))
+
+print(ans)
