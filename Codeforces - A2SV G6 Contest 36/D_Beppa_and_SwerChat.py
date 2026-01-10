18487a9d771ef558a6ddd1494b2aa36b8380306f
kidus-m <kidus0237@gmail.com>
2026-01-10 20:29:50 +0300

    contest

--- Codeforces - A2SV G6 Contest 36/D_Beppa_and_SwerChat.py
+++ Codeforces - A2SV G6 Contest 36/D_Beppa_and_SwerChat.py
@@ -1,2 +1,22 @@
+t = int(input())
 
-// D.Beppa and SwerChat
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    pos = [0] * (n + 1)
+    for i in range(n):
+        pos[a[i]] = i
+
+    offline = 0
+    last = n  # larger than any index
+
+    for i in range(n - 1, -1, -1):
+        if pos[b[i]] < last:
+            offline += 1
+            last = pos[b[i]]
+        else:
+            break
+
+    print(n - offline)
