b28d2fbc1de87360e6e22a1d978143706511bf3e
kidus-m <kidus0237@gmail.com>
2026-01-10 20:25:39 +0300

    contest

--- Codeforces - A2SV G6 Contest 36/B_Make_It_Increasing.py
+++ Codeforces - A2SV G6 Contest 36/B_Make_It_Increasing.py
@@ -1,3 +1,25 @@
-// B.Make
-It
-Increasing
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    ops = 0
+    possible = True
+
+    for i in range(n - 2, -1, -1):
+        if a[i + 1] == 0:
+            possible = False
+            break
+
+        while a[i] >= a[i + 1]:
+            a[i] //= 2
+            ops += 1
+            if a[i] == 0:
+                break
+
+        if a[i] >= a[i + 1]:
+            possible = False
+            break
+
+    print(ops if possible else -1)
