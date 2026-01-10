0fb1050fffaa28a8c7b212556f02ecc762d63764
kidus-m <kidus0237@gmail.com>
2026-01-10 20:33:40 +0300

    contest

--- Codeforces - A2SV G6 Contest 36/E_Moving_Both_Hands.py
+++ Codeforces - A2SV G6 Contest 36/E_Moving_Both_Hands.py
@@ -2,23 +2,17 @@ import heapq
 
 n, m = map(int, input().split())
 
-# Graph with 2 layers
 adj = [[] for _ in range(2 * n + 1)]
 
 for _ in range(m):
     u, v, w = map(int, input().split())
-    # normal edge
     adj[u].append((v, w))
-    # reversed edge in second layer
     adj[v + n].append((u + n, w))
-
-# zero-cost transition to reversed layer
 for i in range(1, n + 1):
     adj[i].append((i + n, 0))
 
-# Dijkstra
 dist = [-1] * (2 * n + 1)
-pq = [(0, 1)]  # (distance, node)
+pq = [(0, 1)]
 
 while pq:
     d, u = heapq.heappop(pq)
@@ -28,6 +22,4 @@ while pq:
     for v, w in adj[u]:
         if dist[v] == -1:
             heapq.heappush(pq, (d + w, v))
-
-# Output distances to nodes 2..n in reversed layer
 print(" ".join(str(dist[i + n]) for i in range(2, n + 1)))
