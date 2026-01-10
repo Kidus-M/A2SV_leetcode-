8da7ebca94018651295ba9f23e1ce5ad9c08d460
kidus-m <kidus0237@gmail.com>
2026-01-10 20:21:24 +0300

    contest

--- Codeforces - A2SV G6 Contest 36/A_False_Alarm.py
+++ Codeforces - A2SV G6 Contest 36/A_False_Alarm.py
@@ -1,3 +1,20 @@
-// A.
-False
-Alarm
+t = int(input())
+
+for _ in range(t):
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    max_closed = 0
+    current = 0
+
+    for door in a:
+        if door == 1:
+            current += 1
+            max_closed = max(max_closed, current)
+        else:
+            current = 0
+
+    if max_closed <= x:
+        print("YES")
+    else:
+        print("NO")
