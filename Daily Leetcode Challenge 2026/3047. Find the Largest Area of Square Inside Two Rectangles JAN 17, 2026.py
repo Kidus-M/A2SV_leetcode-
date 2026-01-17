374134637c6234ee1b2196bab252b7dd131aadcf
kidus-m <kidus0237@gmail.com>
2026-01-17 15:13:25 +0300

    daily

--- Daily Leetcode Challenge 2026/3047. Find the Largest Area of Square Inside Two Rectangles JAN 17, 2026.py
+++ Daily Leetcode Challenge 2026/3047. Find the Largest Area of Square Inside Two Rectangles JAN 17, 2026.py
@@ -4,7 +4,6 @@ class Solution:
         ans = 0
         for i in range(n):
             for j in range(i + 1, n):
-
                 left   = max(bottomLeft[i][0], bottomLeft[j][0])
                 right  = min(topRight[i][0], topRight[j][0])
                 bottom = max(bottomLeft[i][1], bottomLeft[j][1])
