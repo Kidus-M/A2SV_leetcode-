546b91db55b923f0445e6a8700e1417c07effbe8
kidus-m <kidus0237@gmail.com>
2026-01-18 11:20:46 +0300

    feat: Implement solution for LeetCode problem 1895, Largest Magic Square, using prefix sums for efficient checking.

--- Daily Leetcode Challenge 2026/1895. Largest Magic Square JAN 18, 2026.py
+++ Daily Leetcode Challenge 2026/1895. Largest Magic Square JAN 18, 2026.py
@@ -3,14 +3,11 @@ class Solution:
         r, c= len(grid), len(grid[0])
         rowSum=[list(accumulate(row, initial=0)) for row in grid]
         colSum=[list(accumulate(col, initial=0)) for col in zip(*grid)]
-
         diag, antidiag=([[0]*(c+1) for _ in range(r+1)] for _ in range(2))
-
         for i, row in enumerate(grid):
             for j, x in enumerate(row):
                 diag[i+1][j+1]=diag[i][j]+x
                 antidiag[i+1][j]=antidiag[i][j+1]+x
-
         def isMagic(k):
             for i in range(r-k+1):
                 for j in range(c-k+1):
