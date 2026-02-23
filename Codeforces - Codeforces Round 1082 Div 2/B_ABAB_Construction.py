fab7fc9e97a0dc14e0621a335ca81bb8e37fd498
kidus-m <kidus0237@gmail.com>
2026-02-23 17:57:25 +0300

    contest div 2

--- Codeforces - Codeforces Round 1082 Div 2/B_ABAB_Construction.py
+++ Codeforces - Codeforces Round 1082 Div 2/B_ABAB_Construction.py
@@ -1,2 +1,25 @@
-// B.ABAB
-Construction
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = list(input())
+
+    for i in range(n):
+        if s[i] == '?':
+            for c in ('a', 'b'):
+                ok = True
+                if i > 0 and s[i - 1] == c and (i - 1) % 2 == 1:
+                    ok = False
+                if i + 1 < n and s[i + 1] == c and i % 2 == 1:
+                    ok = False
+                if ok:
+                    s[i] = c
+                    break
+
+    good = True
+    for i in range(n - 1):
+        if s[i] == s[i + 1] and i % 2 == 1:
+            good = False
+            break
+
+    print("YES" if good else "NO")
