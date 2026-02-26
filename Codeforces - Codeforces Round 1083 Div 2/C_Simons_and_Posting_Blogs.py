5d508e476396426c8b887800a2d9eaa3e16821c1
kidus-m <kidus0237@gmail.com>
2026-02-26 18:41:45 +0300

    contest

--- Codeforces - Codeforces Round 1083 Div 2/C_Simons_and_Posting_Blogs.py
+++ Codeforces - Codeforces Round 1083 Div 2/C_Simons_and_Posting_Blogs.py
@@ -1,2 +1,53 @@
-// C.Simons and Posting
-Blogs
+t = int(input())
+
+for _ in range(t):
+    bc = int(input())
+
+    blogs = []
+    allu = set()
+
+    for _ in range(bc):
+        arr = list(map(int, input().split()))
+        b = arr[1:]
+        blogs.append(b)
+        for x in b:
+            allu.add(x)
+
+    rem = set(allu)
+    res = []
+
+    while rem:
+        smalltail = 10**18
+
+        for b in blogs:
+            for i in range(len(b) - 1, -1, -1):
+                if b[i] in rem:
+                    smalltail = min(smalltail, b[i])
+                    break
+
+        best = None
+
+        for b in blogs:
+            tail = 10**18
+            for i in range(len(b) - 1, -1, -1):
+                if b[i] in rem:
+                    tail = b[i]
+                    break
+
+            if tail == smalltail:
+                cur = []
+                used = set()
+                for i in range(len(b) - 1, -1, -1):
+                    v = b[i]
+                    if v in rem and v not in used:
+                        cur.append(v)
+                        used.add(v)
+
+                if best is None or cur < best:
+                    best = cur
+
+        for v in best:
+            res.append(v)
+            rem.remove(v)
+
+    print(' '.join(map(str, res)))
