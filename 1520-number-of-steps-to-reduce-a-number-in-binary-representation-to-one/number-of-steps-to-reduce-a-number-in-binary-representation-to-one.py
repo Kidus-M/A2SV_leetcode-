2e8867b5363d61b884d0c9008ef7cf7468761c0a
Kidus Mesfin Teferi <140906551+Kidus-M@users.noreply.github.com>
2026-02-26 08:34:22 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (54.01%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numSteps(self, s: str) -> int:
+        res = 0
+        stack = list(s)
+        carry = 0
+
+
+        while len(stack) != 1:
+            curr = stack.pop()
+
+            if curr == '0' and not carry:
+                res += 1
+            elif curr == '1' and not carry:
+                carry =1
+                res += 2
+            elif curr=='0' and carry:
+                res += 2
+            else:
+                res +=1
+        if stack[0] == '1' and carry:
+            res += 1
+
+        return res
