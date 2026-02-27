eb1dd36f3fbe0a1bbd2689a7ee386e75e980273d
Kidus Mesfin Teferi <140906551+Kidus-M@users.noreply.github.com>
2026-02-27 13:22:24 +0300

    Time: 1058 ms (24%), Space: 28.7 MB (24%) - LeetHub

---
+++ 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.py
@@ -0,0 +1,42 @@
+class Solution:
+    def minOperations(self, s: str, k: int) -> int:
+        n = len(s)
+        z0 = s.count('0')  
+
+        if z0 == 0:
+            return 0  
+        even = list(range(0, n + 1, 2))
+        odd = list(range(1, n + 1, 2))
+        visited = [False] * (n + 1)
+
+        def erase_range(lst, L, R):
+            l = bisect_left(lst, L)
+            r = bisect_right(lst, R)
+            result = lst[l:r]
+            del lst[l:r]
+            return result
+
+        q = deque([(z0, 0)])
+        visited[z0] = True
+
+        while q:
+            z, dist = q.popleft()
+            i_min = max(0, k - (n - z))
+            i_max = min(k, z)
+            L = z + k - 2 * i_max
+            R = z + k - 2 * i_min
+            p = (z + k) % 2
+
+            if p == 0:
+                next_states = erase_range(even, max(0, L), min(n, R))
+            else:
+                next_states = erase_range(odd, max(0, L), min(n, R))
+
+            for z2 in next_states:
+                if not visited[z2]:
+                    if z2 == 0:
+                        return dist + 1
+                    visited[z2] = True
+                    q.append((z2, dist + 1))
+
+        return -1
