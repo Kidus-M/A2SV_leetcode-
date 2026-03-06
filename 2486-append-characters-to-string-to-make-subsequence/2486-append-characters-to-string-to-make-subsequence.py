3cb6685d3b3ce452c56fb887aee44d5ef80040ec
Kidus Mesfin Teferi <140906551+Kidus-M@users.noreply.github.com>
2026-03-06 22:00:33 +0300

    Time: 16 ms (84.37%), Space: 19.9 MB (85.59%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.py
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.py
@@ -1,13 +1,13 @@
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
-        lengthS = len(s)
-        lengthT = len(t)
+        m = len(s)
+        n = len(t)
         
-        Pointer1 = 0
-        Pointer2 = 0
-        while Pointer1 < lengthS and Pointer2 < lengthT:
-            if s[Pointer1] == t[Pointer2]:
-                Pointer2 += 1
-            Pointer1 += 1
+        i = 0
+        j = 0
+        while i < m and j < n:
+            if s[i] == t[j]:
+                j += 1
+            i += 1
         
-        return lengthS - Pointer1
+        return n - j
