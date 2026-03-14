399be3845b8143a10d31a7bf29f7908555c47c8b
kidus-m <kidus0237@gmail.com>
2026-03-14 18:11:35 +0300

    contest

--- Codeforces - Codeforces Round 1086 Div 2/C_Stamina_and_Tasks.py
+++ Codeforces - Codeforces Round 1086 Div 2/C_Stamina_and_Tasks.py
@@ -1,1 +1,21 @@
-// C.Stamina and Tasks
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    c = [0]*n
+    p = [0]*n
+
+    for i in range(n):
+        x, y = map(int, input().split())
+        c[i] = x
+        p[i] = y
+
+    f = 0.0
+    i = n-1
+    while i >= 0:
+        m = 1.0 - p[i]/100.0
+        v = c[i] + m*f
+        if v > f:
+            f = v
+        i -= 1
+
+    print("%.10f" % f)
