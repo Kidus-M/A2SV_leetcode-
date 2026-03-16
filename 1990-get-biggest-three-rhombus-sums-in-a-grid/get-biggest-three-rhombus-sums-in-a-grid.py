cc6179b74e7f216a948c292587b09ab70f73edbb
Kidus Mesfin Teferi <140906551+Kidus-M@users.noreply.github.com>
2026-03-16 17:35:39 +0300

    Time: 480 ms (78.85%) | Memory: 23.4 MB (8.65%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.py
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.py
@@ -1,10 +1,23 @@
 class Solution:
     def getBiggestThree(self, g: List[List[int]]) -> List[int]:
-        m,n,d1,d2,res = len(g),len(g[0]),{},{},{*chain(*g)}
-        for i,j in product(range(m),range(n)):
-            d1[i,j],d2[i,j] = d1.get((i-1,j-1),0)+g[i][j],d2.get((i-1,j+1),0)+g[i][j]
-            res.update(d1[i,j]-d1[i-k,j-k] + d1[i-k,j+k]-d1[i-k*2,j] +
-                d2[i,j]-d2[i-k,j+k] + d2[i-k,j-k]-d2[i-k*2,j] - g[i][j]+g[i-k*2][j]
-                    for k in range(1,min(i//2,j,n-j-1)+1))
+        m, n, d1, d2, res = len(g), len(g[0]), {}, {}, {*chain(*g)}
+        for i, j in product(range(m), range(n)):
+            d1[i, j], d2[i, j] = (
+                d1.get((i - 1, j - 1), 0) + g[i][j],
+                d2.get((i - 1, j + 1), 0) + g[i][j],
+            )
+            res.update(
+                d1[i, j]
+                - d1[i - k, j - k]
+                + d1[i - k, j + k]
+                - d1[i - k * 2, j]
+                + d2[i, j]
+                - d2[i - k, j + k]
+                + d2[i - k, j - k]
+                - d2[i - k * 2, j]
+                - g[i][j]
+                + g[i - k * 2][j]
+                for k in range(1, min(i // 2, j, n - j - 1) + 1)
+            )
 
-        return nlargest(3,res)
+        return nlargest(3, res)
