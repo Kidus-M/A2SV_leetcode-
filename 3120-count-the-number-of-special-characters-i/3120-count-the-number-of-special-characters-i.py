63a6282385faaf0994cee2124b74b1ba9b5cc170
Kidus Mesfin Teferi <140906551+Kidus-M@users.noreply.github.com>
2026-05-26 10:18:15 +0300

    Time: 0 ms (100%), Space: 19.2 MB (64.16%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        letter_counter = [-1]*26
+        result = 0
+        for c in word:
+            if c.islower():
+                idx = ord(c) - ord('a')
+                if letter_counter[idx] == -1:
+                    letter_counter[idx] = 1
+                elif letter_counter[idx] == 2:
+                    letter_counter[idx] = 3
+                    result += 1
+            elif c.isupper():
+                idx = ord(c) - ord('A')
+                if letter_counter[idx] == -1:
+                    letter_counter[idx] = 2
+                elif letter_counter[idx] == 1:
+                    letter_counter[idx] = 3
+                    result += 1
+
+        return result
