d2268a3de1f7a58b26cd249fff513dbb2f55570c
Kidus Mesfin Teferi <140906551+Kidus-M@users.noreply.github.com>
2026-06-20 11:43:20 +0300

    Time: 235 ms (77.27%), Space: 46.1 MB (84.09%) - LeetHub

--- 1840-maximum-building-height/1840-maximum-building-height.py
+++ 1840-maximum-building-height/1840-maximum-building-height.py
@@ -1,21 +1,8 @@
 class Solution:
-    def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
-        restrictions.append([1,0])
-        restrictions.sort()
-        if restrictions[-1][0] != n:
-            restrictions.append([n, n-1])
-        m = len(restrictions)
-        for i in range(1, m):
-            dist = restrictions[i][0] - restrictions[i-1][0]
-            restrictions[i][1] = min(restrictions[i][1], restrictions[i-1][1] + dist)
-        for i in range(m-2, -1, -1):
-            dist = restrictions[i+1][0] - restrictions[i][0]
-            restrictions[i][1] = min(restrictions[i][1], restrictions[i+1][1] + dist)
-        ans = 0
-        for i in range(m-1):
-            id1, h1 = restrictions[i]
-            id2, h2 = restrictions[i+1]
-            dist = id2-id1
-            max_height = (dist+h1+h2) // 2
-            ans = max(ans, max_height)
-        return ans
+    def maxBuilding(self, n: int, re: List[List[int]]) -> int:
+        re.sort(key=lambda i:i[0]+i[1])
+        ans, b = 0, [1,0]
+        for r in re:
+            ans = max(ans,(min(2*n-b[0]+b[1],r[0]+r[1])-(b[0]-b[1]))//2)
+            if b[0]-b[1] < r[0]-r[1]: b = [r[0],r[1]]
+        return max(ans,n-b[0]+b[1])
