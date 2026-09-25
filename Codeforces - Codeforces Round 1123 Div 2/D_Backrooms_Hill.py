42f3627d4e87c493ffb57d6f7709082af5a7e088
kidus-m <kidus0237@gmail.com>
2026-09-25 18:29:51 +0300

    contest after awhile let's see how this one goes

--- Codeforces - Codeforces Round 1123 Div 2/D_Backrooms_Hill.py
+++ Codeforces - Codeforces Round 1123 Div 2/D_Backrooms_Hill.py
@@ -1,2 +1,31 @@
-// D.Backrooms
-Hill
+import sys
+input=sys.stdin.readline
+t=int(input())
+
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+
+    pos=[0]*(n+1)
+    for i in range(n):
+        pos[a[i]]=i%2
+
+    p=pos[n]
+    st={(0,0)}
+    for v in range(n-1,0,-1):
+        c=pos[v]
+        nst=set()
+        for l,r in st:
+            if (p+l+1)%2==c:
+                nst.add(((l+1)%2,r))
+            if (p+r+1)%2==c:
+                nst.add((l,(r+1)%2))
+        st=nst
+        if not st:
+            break
+
+    ok=False
+    for l,r in st:
+        if l==p:
+            ok=True
+    print("YES" if ok else "NO")
