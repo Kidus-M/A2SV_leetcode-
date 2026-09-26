642dede8483d8d2d2f480a0cf66bbb3cac38c806
kidus-m <kidus0237@gmail.com>
2026-09-26 19:47:04 +0300

    contest after awhile let's see how this one goes

--- Codeforces - Codeforces Round 1124 Div 2/D_What_a_SauSaGe_Its_All_Meat.py
+++ Codeforces - Codeforces Round 1124 Div 2/D_What_a_SauSaGe_Its_All_Meat.py
@@ -1,4 +1,24 @@
-// D.What
-a
-SauSaGe! It
-'s All Meat
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+out=[]
+for _ in range(t):
+    n,q=map(int,input().split())
+    a=list(map(int,input().split()))
+
+    ev=[bin(v).count("1")%2==0 for v in a]
+    cnt=sum(ev)
+
+    res=[cnt]
+    for _ in range(q):
+        p,x=map(int,input().split())
+        p -= 1
+        cnt -= ev[p]
+        ev[p]=bin(x).count("1")%2==0
+        cnt += ev[p]
+        res.append(cnt)
+
+    out.append(" ".join(map(str,res)))
+
+print("\n".join(out))
